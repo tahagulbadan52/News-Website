0d7949f5b8682851368d0483de3baab9c4bcd8d3
sms9092 <sms9092@live.com>
2020-08-24 23:12:26 +0400

    category tag and buttons styles

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,13 +6,17 @@ function Article() {
     return (
         <div className="article">
             <img src={pic1} alt="Article cover" className="article__img"/>
-            <div className="article__category">Politics</div>
+            <div className="article__category">
+                <div className="article__category--item">
+                    Politics
+                </div>
+            </div>
             <div className="article__title">India rejects reference to J&K in China-Pakistan joint statement.</div>
             <div className="article__btn-panel">
-                <button className="article__btn">Original Article</button>
-                <button className="article__btn">Read Summary</button>
+                <button className="article__btn article__btn--original">Original Article</button>
+                <button className="article__btn article__btn--summary">Read Summary</button>
             </div>
-            
+
         </div>
     )
 }
